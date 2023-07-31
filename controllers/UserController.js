3f08d04d56589ddac94cf022596f0b857a44b22a
bitsentinel-cell <mehransentinel@gmail.com>
2023-07-31 17:57:22 +0330

    normal user registrration and login is done

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require('../models/Users')
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const registerUser = async (req , res) => {
     const {username , email , password} = req.body;
     if(!username || !email || !password){
@@ -14,12 +15,43 @@ const registerUser = async (req , res) => {
 
     //hash password
     const hashed = await bcrypt.hash(password,10);
-    console.log(hashed);
-    res.json({massage: "user registration"});
+    const user = await  User.create({
+        username,
+        email,
+        password: hashed,
+    });
+    console.log(user);
+    if(user){
+        res.status(201).json({_id: user.id, email: user.email})
+    }else{
+        res.status(400);
+        throw new Error("user data is not valid");
+    }
+
 }
 
 const userLogin = async (req , res) => {
-    res.json({massage: "user login"});
+    const {email , password} = req.body;
+    if( !email || !password){
+        res.status(400)
+        throw new Error( "all fields are mandetory!!")
+    }
+    const user = await User.findOne({email});
+    if(user && (await bcrypt.compare(password , user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                 email: user.email,
+                id: user.id,
+            },
+
+        }, process.env.ACCESS_TOKEN_SECRET,{expiresIn: "1m"})
+        res.status(200).json({accessToken})
+    }else{
+        res.status(401);
+        throw new Error("email or password is not valid");
+    }
+
 }
 const CurrentUser = async (req , res) => {
     res.json({massage: "current user"});
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
+const validateToken = require("../middleware/ValidateTokenHandler");
 
 router.post('/register' , UserController.registerUser);
 router.post('/login' , UserController.userLogin);
-router.get('/current' , UserController.CurrentUser);
+router.get('/current' ,validateToken ,  UserController.CurrentUser);
 
 module.exports = router;
